9808a74512b02f99ba96c1e54aa2e5e65d790b2a
shikihaya <94736168+shikihaya@users.noreply.github.com>
2021-11-22 19:36:02 +0900

    stateの使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 import TwoColorfulMessage from "./components/TwoColorfulMessage";
 const App = () => {
-  const onClickButton = () => alert();
-
+  const onClickCountUp = () => setNum(num + 1);
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>H1です！</h1>
       <ColorfulMessage color="blue" message="お元気ですか" />
       <TwoColorfulMessage color="red">あか～</TwoColorfulMessage>
       <TwoColorfulMessage color="green">みどり～</TwoColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
